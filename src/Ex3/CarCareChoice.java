7fdbff70691fb5607c0e3420dc7de57ff9be744a
KylahRC <ST10435066@rcconnect.edu.za>
2024-09-21 10:55:08 +0000

    Ex3 code commented on

--- src/Ex3/CarCareChoice.java
+++ src/Ex3/CarCareChoice.java
@@ -6,17 +6,21 @@ public class CarCareChoice
 {
     public static void main(String[] args)
     {
-        String[] services = {"Oil Change", "Tire Rotation", "Battery Check", "Brake Inspection"};
+//        declarations
+        String[] servicesOffered = {"Oil Change", "Tire Rotation", "Battery Check", "Brake Inspection"};
         double[] prices = {25.00, 22.00, 15.00, 5.00};
         boolean isMatch = false;
         int matchIndex = 0;
 
+//        place for user to enter what they want
         String selection = JOptionPane.showInputDialog(null,
                 "Please type the name of the service to see its price:\nOil Change\nTire Rotation\nBattery Check\nBrake Inspection");
 
-        for (int i = 0; i < services.length; i++)
+//        loop to cycle through the array
+        for (int i = 0; i < servicesOffered.length; i++)
         {
-            if (selection.equalsIgnoreCase(services[i]))
+//            checks if the input matches anything in the array
+            if (selection.equalsIgnoreCase(servicesOffered[i]))
             {
                 isMatch = true;
                 matchIndex = i;
@@ -27,7 +31,7 @@ public class CarCareChoice
         if (isMatch)
         {
             JOptionPane.showMessageDialog(null,
-                    "You selected " + services[matchIndex] + "\nThe price for that service is $" + prices[matchIndex]);
+                    "You selected " + servicesOffered[matchIndex] + "\nThe price for that service is $" + prices[matchIndex]);
         } else
         {
             JOptionPane.showMessageDialog(null, "Sorry, no selection found!");
--- src/Ex3/CarCareChoice2.java
+++ src/Ex3/CarCareChoice2.java
@@ -6,6 +6,7 @@ public class CarCareChoice2
 {
     public static void main(String[] args)
     {
+//        declarations
         String[] services = {"Oil Change", "Tire Rotation", "Battery Check", "Brake Inspection"};
         double[] prices = {25.00, 22.00, 15.00, 5.00};
         boolean isMatch = false;
@@ -14,8 +15,11 @@ public class CarCareChoice2
         String selection = JOptionPane.showInputDialog(null,
                 "Please type the first three characters of the service to see its price:\nOil Change\nTire Rotation\nBattery Check\nBrake Inspection");
 
+//            checks if the input matches anything in the array
         for (int i = 0; i < services.length; i++)
         {
+//            this gets the first 3 letters in the stuff they entered
+//            tyops never happen in the first 3 letters, obviously
             if (selection.substring(0, 3).equalsIgnoreCase(services[i].substring(0, 3)))
             {
                 isMatch = true;
@@ -28,7 +32,8 @@ public class CarCareChoice2
         {
             JOptionPane.showMessageDialog(null,
                     "You selected " + services[matchIndex] + "\nThe price for that service is $" + prices[matchIndex]);
-        } else
+        }
+        else
         {
             JOptionPane.showMessageDialog(null, "Sorry, no selection found!");
         }
